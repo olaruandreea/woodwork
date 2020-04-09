8fe7d47007f752a83ec1512c3a49624edf5d3482
olaru <olaruandreeapaticia@gmail.com>
2020-04-09 20:49:23 +0100

    recent changes

---
+++ frontend/src/components/navbarComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class navbarComponent extends Component {
+    render() { 
+        return (<h2>HI</h2>  );
+    }
+}
+ 
+export default navbarComponent;
