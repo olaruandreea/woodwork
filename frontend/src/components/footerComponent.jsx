bd978dc7a081ba3c319bf30d214342ae66f22af0
olaru <olaruandreeapaticia@gmail.com>
2020-04-11 20:31:36 +0100

    Added social Icons

--- frontend/src/components/footerComponent.jsx
+++ frontend/src/components/footerComponent.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import '../styles/Footer.css';
+import { SocialIcon } from 'react-social-icons';
+import 'font-awesome/css/font-awesome.min.css';
 
 class Footer extends Component {
     render() { 
@@ -7,49 +10,44 @@ class Footer extends Component {
               <div className="container-fluid text-center text-md-left">
                 <div className="row">
                   <div className="col-md-6 mt-md-0 mt-3">
-                    <h5 className="text-uppercase">Footer Content</h5>
-                    <p>Here you can use rows and columns to organize your footer content.</p>
+                    <h5 className="text-uppercase">About US</h5>
+                    <p></p>
                   </div>
                   <hr className="clearfix w-100 d-md-none pb-3"/>
                   <div className="col-md-3 mb-md-0 mb-3">
-                    <h5 className="text-uppercase">Links</h5>
+                    <h5 className="text-uppercase">Social Links</h5>
                     <ul className="list-unstyled">
-                      <li>
-                        <a href="#!">Link 1</a>
+                      <li className="social-link-button">
+                        <SocialIcon url="https://www.facebook.com/DorrianNPT/?__tn__=%2Cd%2CP-R&eid=ARC9y3Fg2oZfFvzJ73os1cFKuRg8pzbmR3Ap1HesLLgye-6EbAAOJpYEkpqfkQTwA1zxIX53pCunrNXB" network="facebook" style={{ height: 35, width: 35 }} fgColor="white"/>
                       </li>
-                      <li>
-                        <a href="#!">Link 2</a>
+                      <li className="social-link-button">
+                        <SocialIcon url="http://twitter.com" network="twitter" style={{ height: 35, width: 35 }} fgColor="white"/>
                       </li>
-                      <li>
-                        <a href="#!">Link 3</a>
+                      <li className="social-link-button">
+                        <SocialIcon url="http://linkedin.com" network="linkedin" style={{ height: 35, width: 35 }} fgColor="white"/>
                       </li>
-                      <li>
-                        <a href="#!">Link 4</a>
+                      <li className="social-link-button">
+                        <SocialIcon url="http://youtube.com" network="youtube" style={{ height: 35, width: 35 }} fgColor="white"/>
                       </li>
                     </ul>
+
                   </div>  
                   <div className="col-md-3 mb-md-0 mb-3">            
-                    <h5 className="text-uppercase">Links</h5>
+                    <h5 className="text-uppercase">Contact Us</h5>
                     <ul className="list-unstyled">
                       <li>
-                        <a href="#!">Link 1</a>
+                        <a href="#!">23 Sherard Street</a>
                       </li>
                       <li>
-                        <a href="#!">Link 2</a>
+                        <a href="#!">Flat 3</a>
                       </li>
                       <li>
-                        <a href="#!">Link 3</a>
-                      </li>
-                      <li>
-                        <a href="#!">Link 4</a>
+                        <a href="#!">Dublin 1</a>
                       </li>
                     </ul>
                   </div>            
                 </div>            
-              </div>            
-              <div className="footer-copyright text-center py-3">© 2020 Copyright:
-                <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-              </div>            
+              </div>                      
             </footer>
         );
     }
